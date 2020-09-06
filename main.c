84938ade5d38242fdd92090e9a49260c170bd46d
gabihirashima <57654394+gabihirashima@users.noreply.github.com>
2020-09-06 03:54:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -26,7 +26,8 @@ void main(int argc, char *argv[]){
     listaR = insereElemento(listaR, elemento);
     imprimeLista(listaR, 'r');
     elemento = criaRadio(4, 24.7, 5.8, "rosa", "roxo");
-    listaR = insertBefore(listaR, elemento, 1, 'r');
+    listaR = insertBefore(listaR, elemento, 2, 'r');
+    imprimeLista(listaR, 'r');
     tamanho = tamanhoLista(listaR);
     printf("\ntamanho da lista: %d", tamanho);
     liberaLista(listaR);
